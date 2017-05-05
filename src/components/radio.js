3149bbbcfb9c96b73481bacea2d47d1a0236a75b
Sebastian Balay <snbalay@gmail.com>
2017-05-05 11:47:19 -0300

    Support radio buttons with datasource. WIP

--- src/components/radio.js
+++ src/components/radio.js
@@ -1,3 +1,4 @@
+var _clone = require('lodash/clone');
 module.exports = function(app) {
   app.config([
     'formioComponentsProvider',
@@ -11,7 +12,7 @@ module.exports = function(app) {
           },
           {
             name: 'Data',
-            template: 'formio/components/common/data.html'
+            template: 'formio/components/radio/data.html'
           },
           {
             name: 'Validation',
@@ -30,6 +31,109 @@ module.exports = function(app) {
             template: 'formio/components/common/conditional.html'
           }
         ],
+        onEdit: ['$scope', 'FormioUtils', function($scope, FormioUtils) {
+          $scope.dataSources = {
+            values: 'Values',
+            json: 'Raw JSON',
+            url: 'URL',
+            resource: 'Resource',
+            custom: 'Custom'
+          };
+          $scope.resources = [];
+          $scope.resourceFields = [];
+
+          // Returns only input fields we are interested in.
+          var getInputFields = function(components) {
+            var fields = [];
+            FormioUtils.eachComponent(components, function(component) {
+              if (component.key && component.input && (component.type !== 'button') && component.key !== $scope.component.key) {
+                var comp = _clone(component);
+                if (!comp.label) {
+                  comp.label = comp.key;
+                }
+                fields.push(comp);
+              }
+            });
+            return fields;
+          };
+
+          $scope.formFields = [{label: 'Any Change', key: 'data'}].concat(getInputFields($scope.form.components));
+
+          // Loads the selected fields.
+          var loadFields = function() {
+            if (!$scope.component.data.resource || ($scope.resources.length === 0)) {
+              return;
+            }
+            var selected = null;
+            $scope.resourceFields = [
+              {
+                property: '',
+                title: '{Entire Object}'
+              },
+              {
+                property: '_id',
+                title: 'Submission Id'
+              }
+            ];
+            if ($scope.formio.projectId) {
+              $scope.component.data.project = $scope.formio.projectId;
+            }
+            for (var index in $scope.resources) {
+              if ($scope.resources[index]._id.toString() === $scope.component.data.resource) {
+                selected = $scope.resources[index];
+                break;
+              }
+            }
+            if (selected) {
+              var fields = getInputFields(selected.components);
+              for (var i in fields) {
+                var field = fields[i];
+                var title = field.label || field.key;
+                $scope.resourceFields.push({
+                  property: 'data.' + field.key,
+                  title: title
+                });
+              }
+              if (!$scope.component.valueProperty && $scope.resourceFields.length) {
+                $scope.component.valueProperty = $scope.resourceFields[0].property;
+              }
+            }
+          };
+
+          $scope.$watch('component.dataSrc', function(source) {
+            if (($scope.resources.length === 0) && (source === 'resource')) {
+              $scope.formio.loadForms({params: {type: 'resource', limit: 4294967295}}).then(function(resources) {
+                $scope.resources = resources;
+                loadFields();
+              });
+            }
+          });
+
+          // Trigger when the resource changes.
+          $scope.$watch('component.data.resource', function(resourceId) {
+            if (!resourceId) {
+              return;
+            }
+            loadFields();
+          });
+
+          // Update other parameters when the value property changes.
+          $scope.currentValueProperty = $scope.component.valueProperty;
+          $scope.$watch('component.valueProperty', function(property) {
+            if ($scope.component.dataSrc === 'resource' && $scope.currentValueProperty !== property) {
+              if (!property) {
+                $scope.component.searchField = '';
+                $scope.component.template = '<span>{{ item.data }}</span>';
+              }
+              else {
+                $scope.component.searchField = property + '__regex';
+                $scope.component.template = '<span>{{ item.' + property + ' }}</span>';
+              }
+            }
+          });
+
+          loadFields();
+        }],
         documentation: 'http://help.form.io/userguide/#radio'
       });
     }
@@ -41,7 +145,6 @@ module.exports = function(app) {
       $templateCache.put('formio/components/radio/display.html',
         '<ng-form>' +
           '<form-builder-option property="label"></form-builder-option>' +
-          '<value-builder data="component.values" default="component.defaultValue" label="Values" tooltip-text="The radio button values that can be picked for this field. Values are text submitted with the form data. Labels are text that appears next to the radio buttons on the form."></value-builder>' +
           '<form-builder-option property="customClass"></form-builder-option>' +
           '<form-builder-option property="tabindex"></form-builder-option>' +
           '<form-builder-option property="inline" type="checkbox" label="Inline Layout" title="Displays the radio buttons horizontally."></form-builder-option>' +
@@ -58,6 +161,57 @@ module.exports = function(app) {
         '<ng-form>' +
           '<form-builder-option property="validate.required"></form-builder-option>' +
           '<form-builder-option-custom-validation></form-builder-option-custom-validation>' +
+        '</ng-form>'
+      );
+
+      $templateCache.put('formio/components/radio/data.html',
+        '<ng-form>' +
+          '<div class="form-group">' +
+            '<label for="dataSrc" form-builder-tooltip="The source to use for the radio options. Values lets you provide your own values and labels. JSON lets you provide raw JSON data. URL lets you provide a URL to retrieve the JSON data from.">Data Source Type</label>' +
+            '<select class="form-control" id="dataSrc" name="dataSrc" ng-model="component.dataSrc" ng-options="value as label for (value, label) in dataSources"></select>' +
+          '</div>' +
+          '<ng-switch on="component.dataSrc">' +
+            '<div class="form-group" ng-switch-when="json">' +
+              '<label for="data.json" form-builder-tooltip="A raw JSON array to use as a data source.">Data Source Raw JSON</label>' +
+              '<textarea class="form-control" id="data.json" name="data.json" ng-model="component.data.json" placeholder="Raw JSON Array" json-input rows="3">{{ component.data.json }}</textarea>' +
+            '</div>' +
+            '<form-builder-option ng-switch-when="url" property="data.url" label="Data Source URL" placeholder="Data Source URL" title="A URL that returns a JSON array to use as the data source."></form-builder-option>' +
+            '<value-builder ng-switch-when="values" data="component.data.values" label="Data Source Values" tooltip-text="Values to use as the data source. Labels are shown in the select field. Values are the corresponding values saved with the submission."></value-builder>' +
+          '<div class="form-group" ng-switch-when="resource">' +
+            '<label for="placeholder" form-builder-tooltip="The resource to be used with this field.">Resource</label>' +
+            '<ui-select ui-select-required ui-select-open-on-focus ng-model="component.data.resource" theme="bootstrap">' +
+              '<ui-select-match class="ui-select-match" placeholder="">' +
+                '{{$select.selected.title}}' +
+              '</ui-select-match>' +
+              '<ui-select-choices class="ui-select-choices" repeat="value._id as value in resources | filter: $select.search" refresh="refreshSubmissions($select.search)" refresh-delay="250">' +
+                '<div ng-bind-html="value.title | highlight: $select.search"></div>' +
+              '</ui-select-choices>' +
+            '</ui-select>' +
+          '</div>' +
+          '</ng-switch>' +
+          '<form-builder-option ng-hide="component.dataSrc !== \'url\'" property="selectValues" label="Data Path" type="text" placeholder="The object path to the iterable items." title="The property within the source data, where iterable items reside. For example: results.items or results[0].items"></form-builder-option>' +
+          '<form-builder-option ng-hide="component.dataSrc == \'values\' || component.dataSrc == \'resource\' || component.dataSrc == \'custom\'" property="valueProperty" label="Value Property" placeholder="The selected item\'s property to save." title="The property of each item in the data source to use as the select value. If not specified, the item itself will be used."></form-builder-option>' +
+          '<form-builder-option ng-hide="component.dataSrc == \'values\' || component.dataSrc == \'resource\' || component.dataSrc == \'custom\'" property="labelProperty" label="Label Property" placeholder="The selected item\'s property to display." title="The property of each item in the data source to use as the label. If not specified, the item itself will be used."></form-builder-option>' +
+          '<div class="form-group" ng-hide="component.dataSrc !== \'resource\' || !component.data.resource || resourceFields.length == 0">' +
+            '<label for="placeholder" form-builder-tooltip="The field to use as the value.">Value</label>' +
+            '<select class="form-control" id="valueProperty" name="valueProperty" ng-options="value.property as value.title for value in resourceFields" ng-model="component.valueProperty"></select>' +
+          '</div>' +
+          '<div class="form-group" ng-if="component.dataSrc == \'resource\' && component.valueProperty === \'\'">' +
+          '  <label for="placeholder" form-builder-tooltip="The properties on the resource to return as part of the options. Separate property names by commas. If left blank, all properties will be returned.">Select Fields</label>' +
+          '  <input type="text" class="form-control" id="selectFields" name="selectFields" ng-model="component.selectFields" placeholder="Comma separated list of fields to select." value="{{ component.selectFields }}">' +
+          '</div>' +
+          '<form-builder-option ng-show="component.dataSrc == \'url\' || component.dataSrc == \'resource\'" property="filter" label="Filter Query" placeholder="The filter query for results." title="Use this to provide additional filtering using query parameters."></form-builder-option>' +
+          '<div class="form-group" ng-show="component.dataSrc == \'custom\'">' +
+          '  <label for="custom" form-builder-tooltip="Write custom code to return the value options. The form data object is available.">Custom Values</label>' +
+          '  <textarea class="form-control" rows="10" id="custom" name="custom" ng-model="component.data.custom" placeholder="/*** Example Code ***/\nvalues = data[\'mykey\'];">{{ component.data.custom }}</textarea>' +
+          '</div>' +
+          '<div class="form-group" ng-hide="component.dataSrc == \'values\' || component.dataSrc == \'json\'">' +
+          '  <label for="placeholder" form-builder-tooltip="Refresh data when another field changes.">Refresh On</label>' +
+          '  <select class="form-control" id="refreshOn" name="refreshOn" ng-options="field.key as field.label for field in formFields" ng-model="component.refreshOn"></select>' +
+          '</div>' +
+          '<form-builder-option ng-show="component.dataSrc == \'resource\' || component.dataSrc == \'url\' || component.dataSrc == \'custom\'" property="clearOnRefresh"></form-builder-option>' +
+          '<form-builder-option ng-show="component.dataSrc == \'url\'" property="authenticate"></form-builder-option>' +
+          '<form-builder-option property="defaultValue"></form-builder-option>' +
         '</ng-form>'
       );
     }
